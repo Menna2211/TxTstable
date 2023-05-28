63faa435f0078e6a8bc0b764d44dac9853998f2f
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-28 16:54:39 +0300

    Stable-Diffusion-Huggingface.py

--- pages/Stable-Diffusion-Huggingface.py
+++ pages/Stable-Diffusion-Huggingface.py
@@ -3,13 +3,12 @@ import torch
 import time
 from optimum.onnxruntime import ORTStableDiffusionPipeline
 
-@st.cache_resource(show_spinner=False ,ttl=3600) 
-def get_model():
-    model_id = "runwayml/stable-diffusion-v1-5"
-    pipe = ORTStableDiffusionPipeline.from_pretrained(model_id, framework="pt")
-    return pipe
+#@st.cache_resource(show_spinner=False ,ttl=3600) 
+#def get_model():
+model_id = "runwayml/stable-diffusion-v1-5"
+pipe = ORTStableDiffusionPipeline.from_pretrained(model_id, framework="pt")
+    #return pipe
 
-pipe=get_model()
 
 st.title("Stable Diffusion App")
 # define the layout of your app
