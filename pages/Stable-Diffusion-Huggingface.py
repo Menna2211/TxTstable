48e1cace2053d0b0aefd24cc4fcb11d902e97ab4
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-28 16:29:51 +0300

    Stable-Diffusion-Huggingface.py

--- pages/Stable-Diffusion-Huggingface.py
+++ pages/Stable-Diffusion-Huggingface.py
@@ -3,8 +3,13 @@ import torch
 import time
 from optimum.onnxruntime import ORTStableDiffusionPipeline
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = ORTStableDiffusionPipeline.from_pretrained(model_id, export=True)
+@st.cache_resource(show_spinner=False ,ttl=3600) 
+def get_model():
+    model_id = "runwayml/stable-diffusion-v1-5"
+    pipe = ORTStableDiffusionPipeline.from_pretrained(model_id, export=True)
+    return pipe
+
+pipe=get_model()
 
 st.title("Stable Diffusion App")
 # define the layout of your app
@@ -12,6 +17,10 @@ st.title("Stable Diffusion App")
 # Define the Streamlit app layout
 prompt = st.text_input("Write your sentence:")
 submit_button = st.button("Compute")
+if not submit_button:
+  time.sleep(3)
+  st.warning('Please Press Compute....')
+  st.stop()
 
 # Display the generated text
 if submit_button:
