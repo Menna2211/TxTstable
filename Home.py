a22b4be700ab3c4688ab69252b990efac4affe0f
Menna2211 <101019260+Menna2211@users.noreply.github.com>
2023-05-29 12:55:18 +0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ st.markdown(
     """
     ### TxTStable using Stable Diffusion:
     The application allows users to input a piece of text and generate an image that is related to the input text. 
-    - Hugging Face Model: [openjourney](https://huggingface.co/prompthero/openjourney?text=man+riding+horse)
+    - Hugging Face Model: [stable-diffusion-v1-5](https://huggingface.co/runwayml/stable-diffusion-v1-5)
     - Github model: [github](https://github.com)
 """
 )
